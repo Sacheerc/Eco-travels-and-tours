8c35e5cdf542d6b2fa12184d6dd47c266fd39294
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-03-28 18:03:21 +0530

    Created admin components

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

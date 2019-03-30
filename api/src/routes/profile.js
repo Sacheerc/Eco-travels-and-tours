56f4a4737539124a283516d5b17db5cf00fbee86
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-03-30 14:10:50 +0530

    Init mongodb with back end

---
+++ api/src/routes/profile.js
@@ -0,0 +1,31 @@
+// This is a sample file which used for development. Should be changed as necessary.
+
+// required modules
+var express = require("express");
+var router = express.Router();
+var User = require("c:/Users/Hp/Desktop/back-end/src/models/user");
+
+// POST route for logging
+router.get("/", function(req, res, next) {
+  User.findById(req.session.userId).exec(function(error, user) {
+    if (error) {
+      return next(error);
+    } else {
+      if (user === null) {
+        var err = new Error("Not authorized! Go back!");
+        err.status = 400;
+        return next(err);
+      } else {
+        return res.send(
+          "<h1>Name: </h1>" +
+            user.username +
+            "<h2>Mail: </h2>" +
+            user.email +
+            '<br><a type="button" href="/logout">Logout</a>'
+        );
+      }
+    }
+  });
+});
+
+module.exports = router;

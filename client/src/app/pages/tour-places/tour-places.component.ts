e4609f752ff4154de4486a4d69c10134a87b2b3f
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-04-27 12:44:56 +0530

    complete data binding in tors-tours

--- client/src/app/pages/tour-places/tour-places.component.ts
+++ client/src/app/pages/tour-places/tour-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TourPackagesService } from '../../services/tours/tour-packages.service';
 
@@ -12,13 +12,11 @@ import { TourPackagesService } from '../../services/tours/tour-packages.service'
 export class TourPlacesComponent implements OnInit {
 
   package:any;
-
   constructor(private route: ActivatedRoute, private tourpackageservice:TourPackagesService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(param => {
       const id = param.get('id');
-      console.log(id);
       this.tourpackageservice.getpackages().subscribe((result)=>{
         this.package=result
         this.package=this.package.find(tourPackage => tourPackage._id == id);
@@ -30,6 +28,20 @@ export class TourPlacesComponent implements OnInit {
        ) 
     });
   }
+  // ngAfterViewInit() {
+  //   require("src/assets/js/jquery.min.js");
+  //   require("src/assets/js/jquery.easing.1.3.js");
+  //   require("src/assets/js/bootstrap.min.js");
+  //   require("src/assets/js/jquery.waypoints.min.js");
+  //   require("src/assets/js/jquery.flexslider-min.js");
+  //   require("src/assets/js/owl.carousel.min.js");
+  //   require("src/assets/js/jquery.magnific-popup.min.js");
+  //   require("src/assets/js/magnific-popup-options.js");
+  //   require("src/assets/js/bootstrap-datepicker.js");
+  //   require("src/assets/js/jquery.stellar.min.js");
+  //   require("src/assets/js/main.js");
+  // }
+
   public getpackage(){
     return this.package
   }

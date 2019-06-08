698262565dc899691dd341cc51daaa582438f073
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2019-06-08 22:11:11 +0530

    index view rating

--- client/src/app/shared/components/index-reviews/index-reviews.component.ts
+++ client/src/app/shared/components/index-reviews/index-reviews.component.ts
@@ -29,4 +29,6 @@ export class IndexReviewsComponent implements OnInit {
     });
   }
 
+
+
 }
---
+++ client/src/app/shared/components/index-reviews/rateGuide.ts
@@ -0,0 +1,7 @@
+export interface RateGuide{
+    nic?:string;
+    description?: string; 
+    rate?:number;
+    cusname?: String;
+    email?:string;
+}
